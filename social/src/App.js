a23be43f9ad7106a117b4c4ca51d1b4f2309b2c9
NhatTruong-dotnet <builehoangnhattruong@gmail.com>
2021-12-20 16:36:35 +0700

    navbar - header --done

--- social/src/App.js
+++ social/src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
+
 import {Blog, Footer,Possibility, Header, Features,WhatGPT3} from './containers/index';
 import {Navbar, Brand,CTA} from './components/index';
-
+import "./index.css";
+import './App.css';
 const App = () => {
   return (<div className="App">
-    <div className="gradient_bg">
+    <div className="gradient__bg">
        <Navbar />
       <Header />
     </div>
