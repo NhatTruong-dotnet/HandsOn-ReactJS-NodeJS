fcd877292fb3c20e91d81d2a5edb6f2c111c7246
NhatTruong-dotnet <builehoangnhattruong@gmail.com>
2021-12-20 09:42:21 +0700

    init vendor

---
+++ social/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import {Article, Brand, CTA, Feature, Navbar} from './components/index';
+function App() {
+  return (
+    <Article />
+  );
+}
+
+export default App;
